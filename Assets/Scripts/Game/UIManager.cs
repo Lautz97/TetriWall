fb6a4043c90ac5e5dc410d3c1ee502506d5999ee
Lautz97 <davide.lauterio@gmail.com>
2021-05-03 16:37:41 +0200

    materials

--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private TMP_Text text;
+    [SerializeField] private GameObject menuPanel, pausePanel, loadingPanel, gameloopPanel, gameoverPanel;
 
     private void Start()
     {
@@ -23,6 +24,40 @@ public class UIManager : Singleton<UIManager>
     {
         int points = SaveLoad.LoadCurrentScore();
         text.text = "Points: " + points;
+    }
+
+    private void DisableAllPanels()
+    {
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    private void ActivateMainMenu()
+    {
+        DisableAllPanels();
+        menuPanel.SetActive(true);
+    }
+    private void AcivatePause()
+    {
+        DisableAllPanels();
+        pausePanel.SetActive(true);
+    }
+    private void ActivateGameLoop()
+    {
+        DisableAllPanels();
+        gameloopPanel.SetActive(true);
+    }
+    private void ActivateGameOver()
+    {
+        DisableAllPanels();
+        gameoverPanel.SetActive(true);
+    }
+    private void ActivateLoading()
+    {
+        DisableAllPanels();
+        loadingPanel.SetActive(true);
     }
 
 }
